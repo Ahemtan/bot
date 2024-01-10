489f4e90f2b29cde1969c03bcedeccbadedc9059
ahemtan <ahemtan1045@gmail.com>
2024-01-10 17:45:59 +0545

    Bug Fix 1

--- src/commands/games/rps.js
+++ src/commands/games/rps.js
@@ -6,11 +6,23 @@ const {
   ButtonStyle,
 } = require("discord.js");
 
-const choices = [
-  { name: "Rock", emoji: "🪨", beats: "Scissors" },
-  { name: "Paper", emoji: "📄", beats: "Rock" },
-  { name: "Scissors", emoji: "✂️", beats: "Paper" },
+const choices = [{
+    name: "Rock",
+    emoji: "🪨",
+    beats: "Scissors"
+  },
+  {
+    name: "Paper",
+    emoji: "📄",
+    beats: "Rock"
+  },
+  {
+    name: "Scissors",
+    emoji: "✂️",
+    beats: "Paper"
+  },
 ];
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("rps")
@@ -20,113 +32,125 @@ module.exports = {
     ),
 
   async execute(interaction, client) {
-    const targetUser = interaction.options.getUser("user");
+    try {
+      const targetUser = interaction.options.getUser("user");
 
-    if (interaction.user.id === targetUser.id) {
-      interaction.reply({
-        content: "You can't play with yourself. 😢",
-        ephemeral: true,
-      });
-      return;
-    }
-
-    if (targetUser.bot) {
-      interaction.reply({
-        content: "You can't play with bot. 🤖",
-        ephemeral: true,
-      });
-      return;
-    }
-
-    const embed = new EmbedBuilder()
-      .setTitle("Rock Paper Scissors")
-      .setDescription(`${targetUser}'s turn.`)
-      .setColor("Random")
-      .setTimestamp(new Date());
-
-    const buttons = choices.map((choice) => {
-      return new ButtonBuilder()
-        .setCustomId(choice.name)
-        .setLabel(choice.name)
-        .setStyle(ButtonStyle.Primary)
-        .setEmoji(choice.emoji);
-    });
-
-    const row = new ActionRowBuilder().addComponents(buttons);
-
-    const reply = await interaction.reply({
-      content: `${targetUser}, you have been invited to play Rock Paper Scissors!,\n by ${interaction.user}. CLick your choice to start playing!`,
-      embeds: [embed],
-      components: [row],
-    });
-
-    const targetUserInteraction = await reply
-      .awaitMessageComponent({
-        filter: (i) => i.user.id == targetUser.id,
-        time: 30_000,
-      })
-      .catch(async (error) => {
-        embed.setDescription(`${targetUser} did not respond in time.😢`);
-        await reply.edit({ embeds: [embed], components: [] });
+      if (interaction.user.id === targetUser.id) {
+        interaction.reply({
+          content: "You can't play with yourself. 😢",
+          ephemeral: true,
+        });
         return;
-      });
+      }
 
-    if(!targetUserInteraction) return;
-
-    const targetUserChoice = choices.find(
-      (choice) => choice.name === targetUserInteraction.customId,
-    );
-
-    await targetUserInteraction.reply({
-      content: `You picked ${targetUserChoice.name + targetUserChoice.emoji}`,
-      ephemeral: true
-    });
-
-    embed.setDescription(`It's ${interaction.user}'s turn`);
-
-    await reply.edit({
-      content: `${interaction.user} it's your turn!`
-    });
-
-    const initialUserInteraction = await reply
-      .awaitMessageComponent({
-        filter: (i) => i.user.id == interaction.user.id,
-        time: 30_000,
-      })
-      .catch(async (error) => {
-        embed.setDescription(`${interaction.user} did not respond in time.😢`);
-        await reply.edit({ embeds: [embed], components: [] });
+      if (targetUser.bot) {
+        interaction.reply({
+          content: "You can't play with bot. 🤖",
+          ephemeral: true,
+        });
         return;
+      }
+
+      const embed = new EmbedBuilder()
+        .setTitle("Rock Paper Scissors")
+        .setDescription(`${targetUser}'s turn.`)
+        .setColor("Random")
+        .setTimestamp(new Date());
+
+      const buttons = choices.map((choice) => {
+        return new ButtonBuilder()
+          .setCustomId(choice.name)
+          .setLabel(choice.name)
+          .setStyle(ButtonStyle.Primary)
+          .setEmoji(choice.emoji);
       });
 
-    if(!initialUserInteraction) return;
+      const row = new ActionRowBuilder().addComponents(buttons);
 
-    const initialUserChoice = choices.find(
-      (choice) => choice.name === initialUserInteraction.customId
-    );
+      const reply = await interaction.reply({
+        content: `${targetUser}, you have been invited to play Rock Paper Scissors!,\n by ${interaction.user}. CLick your choice to start playing!`,
+        embeds: [embed],
+        components: [row],
+      });
 
-    let result;
+      const targetUserInteraction = await reply
+        .awaitMessageComponent({
+          filter: (i) => i.user.id == targetUser.id,
+          time: 30_000,
+        })
+        .catch(async (error) => {
+          embed.setDescription(`${targetUser} did not respond in time.😢`);
+          await reply.edit({
+            embeds: [embed],
+            components: []
+          });
+          return;
+        });
 
-    if(targetUserChoice.beats === initialUserChoice.name) {
-      result = `${targetUser} won 🎉🥳`;
-    }
+      if (!targetUserInteraction) return;
 
-    if(initialUserChoice.beats === targetUserChoice.name) {
-      result = `${interaction.user} won 🎉🥳`;
-    }
+      const targetUserChoice = choices.find(
+        (choice) => choice.name === targetUserInteraction.customId,
+      );
 
-    if(targetUserChoice.name = initialUserChoice.name) {
-      result = "It was a tie! 😲"
-    }
+      await targetUserInteraction.reply({
+        content: `You picked ${targetUserChoice.name + targetUserChoice.emoji}`,
+        ephemeral: true
+      });
 
-    embed.setDescription(
-      `${targetUser} picked ${targetUserChoice.name + targetUserChoice.emoji}\n
+      embed.setDescription(`It's ${interaction.user}'s turn`);
+
+      await reply.edit({
+        content: `${interaction.user} it's your turn!`
+      });
+
+      const initialUserInteraction = await reply
+        .awaitMessageComponent({
+          filter: (i) => i.user.id == interaction.user.id,
+          time: 30_000,
+        })
+        .catch(async (error) => {
+          embed.setDescription(`${interaction.user} did not respond in time.😢`);
+          await reply.edit({
+            embeds: [embed],
+            components: []
+          });
+          return;
+        });
+
+      if (!initialUserInteraction) return;
+
+      const initialUserChoice = choices.find(
+        (choice) => choice.name === initialUserInteraction.customId
+      );
+
+      let result;
+
+      if (targetUserChoice.beats === initialUserChoice.name) {
+        result = `${targetUser} won 🎉🥳`;
+      }
+
+      if (initialUserChoice.beats === targetUserChoice.name) {
+        result = `${interaction.user} won 🎉🥳`;
+      }
+
+      if (targetUserChoice.name = initialUserChoice.name) {
+        result = "It was a tie! 😲"
+      }
+
+      embed.setDescription(
+        `${targetUser} picked ${targetUserChoice.name + targetUserChoice.emoji}\n
         ${interaction.user} picked ${initialUserChoice.name + initialUserChoice.emoji} \n\n
         ${result}
       `
-    )
+      )
 
-    reply.edit({ embeds: [embed], components: [] });
-
+      reply.edit({
+        embeds: [embed],
+        components: []
+      });
+    } catch (error) {
+      console.log(`Error with choices! ${error}`);
+    }
   },
 };
