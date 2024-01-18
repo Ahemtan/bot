14b3da34aeaa07acef40a477dc952b6e0a160477
ahemtan <ahemtan1045@gmail.com>
2024-01-18 19:32:05 +0545

    Bug Fixed on help cmd

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,5 +3,6 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is done cooking! 😏`);
+        await client.application.commands.set(client.commandArray)
     }
 }
--- src/functions/handlers/handleCommand.js
+++ src/functions/handlers/handleCommand.js
@@ -1,7 +1,14 @@
-const { REST, Routes } = require('discord.js');
+const {
+  REST,
+  Routes
+} = require('discord.js');
 const fs = require("fs");
 
 module.exports = (client) => {
+  const {
+    commandArray
+  } = client
+
   client.handleCommands = async () => {
     const commandFolders = fs.readdirSync("./src/commands");
     for (const folder of commandFolders) {
@@ -9,35 +16,44 @@ module.exports = (client) => {
         .readdirSync(`./src/commands/${folder}`)
         .filter((file) => file.endsWith(".js"));
 
-      const { commands, commandArray } = client;
+      const {
+        commands,
+        commandArray
+      } = client;
 
       for (const file of commandFiles) {
 
         const command = require(`../../commands/${folder}/${file}`);
 
-        const porpoties = { folder, ...command }
-        
+        const porpoties = {
+          folder,
+          ...command
+        }
+
         commands.set(command.data.name, porpoties);
         commandArray.push(command.data.toJSON());
-      
+
       }
     }
 
     const clientId = "1032958576882352148";
     const guildId = "1002584943064531024";
 
-    const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+    const rest = new REST({
+      version: "10"
+    }).setToken(process.env.TOKEN);
 
     (async () => {
       try {
         console.log(`Started refreshing ${client.commandArray.length} application (/) commands.`);
-    
+
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
-          Routes.applicationGuildCommands(clientId, guildId),
-          { body: client.commandArray },
+          Routes.applicationGuildCommands(clientId, guildId), {
+            body: client.commandArray
+          },
         );
-    
+
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
       } catch (error) {
         // And of course, make sure you catch and log any errors!
@@ -46,4 +62,3 @@ module.exports = (client) => {
     })();
   };
 };
-
